7a4624d5eabc95b51fa7f8e9aa55fef4424dae07
Lukasz Sarnacki <lukesarnacki@gmail.com>
2016-09-24 16:46:05 +0200

    setting s3 for paperclip

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,17 @@ module StreetIsYoursBackend
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+
+    if Settings.paperclip.storage.to_s == 's3'
+      config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_credentials => {
+          :bucket => ENV['AWS_BUCKET'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+        },
+        :s3_region => ENV['AWS_REGION']
+      }
+    end
   end
 end
--- config/initializers/carrierwave.rb
+++
@@ -1,6 +0,0 @@
-if Settings.paperclip.storage.to_s == 'fog'
-  config.paperclip_defaults = {
-    storage: :fog,
-    fog_credentials: { fog_directory: ENV['fog_directory'] || 'street-is-yours' }
-  }
-end
