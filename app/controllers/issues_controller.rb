d803c7c4a18e735e59bc1182540786df49370e66
Ale Paredes <aept7714@gmail.com>
2016-09-25 13:22:02 +0200

    Add fix button

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,6 +2,21 @@ class IssuesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @issues = Issue.all.where.not(latitude: nil, longitude: nil)
+    @issues = Issue.all.where.not(latitude: nil, longitude: nil).order(created_at: :desc)
+    @recent_issue = @issues.last
+  end
+
+  def update
+    @issue = Issue.find(params[:id])
+    @issue.update(issue_params)
+    redirect_to issues_path
+  end
+
+  private
+
+  def issue_params
+    params.require(:issue).permit(:state).tap do |whitelisted|
+      whitelisted[:state] = params[:issue][:state].to_i
+    end
   end
 end
